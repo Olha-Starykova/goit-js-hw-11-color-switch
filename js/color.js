2ec4279064446e6b1fc795679a57adbe5d5677af
Olha-Starykova <olenkalog@gmail.com>
2021-02-01 01:58:31 +0200

    com

--- js/color.js
+++ js/color.js
@@ -17,7 +17,59 @@ const colors = [
 // Для генерации случайного числа (индекс элемента массива цветов), 
 //используй функцию randomIntegerFromInterval.
 
+
+const refs = {
+  body: document.querySelector("body"),
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]')
+}
+
+console.dir(refs.body);
+console.dir(refs.start);
+console.dir(refs.stop);
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let intervalId = 0;
+
+const colorChange = () => {
+  let color = randomIntegerFromInterval(0, colors.length - 1);
+   refs.body.style.backgroundColor = colors[color]
+ }
+
+const start = () => {
+  refs.start.disabled = true;
+  intervalId = setInterval(colorChange, 1000)
+
+};
+
+const stop = () => {
+  clearInterval(intervalId);
+  refs.start.disabled = false;
+  };
+
+refs.start.addEventListener('click', start);
+refs.stop.addEventListener('click', stop);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
